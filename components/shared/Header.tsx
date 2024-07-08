7dcf32a411d6f9eb5dd48b866175b5bef8a59c10
Priyanshu Negi <priyanshunegi752@gmail.com>
2024-07-08 15:34:33 +0530

    latest

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src="/assets/images/testing.svg" width={128} height={38}
+            src="/assets/images/latest.png" width={128} height={38}
             alt="Evently logo" 
           />
         </Link>
